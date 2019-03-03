df3ca06889883abb2631c9c517428b6a887f8af0
gvespasiano <gvespasiano@gmail.com>
2019-03-03 16:44:39 -0600

    Donate button

--- script.js
+++ script.js
@@ -21,31 +21,21 @@ var churchillSpeech = {
     userNamePrompt,
     favoriteSpeechPrompt;
 
-//Looping console log of speeches
+//While loop console log of speeches
 	i = 0;
 	while (i<speechesArray.length){
-		console.log("This speech is written by "+speechesArray[i].author)
+		console.log("This is written by "+speechesArray[i].author);
 		i++;
 	};
 	
-
 document.getElementById('BtnDonate').addEventListener('click', function(){
-  //Code in here executes when the user clicks the "Donate" button.
+  //For loop of favorites
   favoriteSpeechPrompt = window.prompt('Which speech author is your favorite?');
 
-  switch(favoriteSpeechPrompt){
-    case 'Churchill':
-      console.log(speechesArray[0].author + ' was ' + speechesArray[0].authorAge + ' during this speech.');
-      break;
-    case 'Ghandi':
-      console.log(speechesArray[1].author + ' was ' + speechesArray[1].authorAge + ' during this speech.');
-      break;
-    case 'Demosthenes':
-      console.log(speechesArray[2].author + ' was ' + speechesArray[2].authorAge + ' during this speech.');
-      break;
-    default:
-      console.log('Did you spell that name correctly?');
-      break;
+  for(var i = 0; i < speechesArray.length; i++) {
+	  if(favoriteSpeechPrompt === speechesArray[i].author){
+	  	console.log(speechesArray[i].author + " was " + speechesArray[i].authorAge + " when he wrote this speech!")
+	  }
   }
 });
 
